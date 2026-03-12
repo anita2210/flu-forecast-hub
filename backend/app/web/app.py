d1ec594215d00f335e7b2ee8fd05262dfff8707b
anita2210 <anitajanie2202@gmail.com>
2026-03-12 18:54:37 -0400

    change in dashboard

--- backend/app/web/app.py
+++ backend/app/web/app.py
@@ -64,7 +64,7 @@ def forecast():
 @app.route('/dashboard')
 def dashboard():
     """Embedded Tableau dashboard."""
-    tableau_url = "https://public.tableau.com/views/FluForecastHub/Overview?:language=en-GB&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link"
+    tableau_url = "https://public.tableau.com/app/profile/anita.janie.christdoss.chelladurai/viz/FluForecastHub/Overview"
     return render_template('dashboard.html', tableau_url=tableau_url)
 
 
